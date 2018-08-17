6562e71c61ef357fa473920562fbd915bdf7e5fe
imclydeshelton <36463308+robertsoaa@users.noreply.github.com>
2018-08-17 19:45:37 -0400

    GitHub Commit for grading

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ var http = require('http');
 var app = express(); 
 var PORT = process.env.PORT || 3000;
 
-//bodyParser added to make server understand data that is sent to it
+//bodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-//points are server to our route files
+//require server to route files
 require('./app/routing/api-routes.js')(app); 
 require('./app/routing/html-routes.js')(app);
 
-//starts our server
+//starts server
 app.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
 });
